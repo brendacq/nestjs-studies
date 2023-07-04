bfbce70d732880243086b437245da72355f5f9b7
Brenda <breeeendaqn@gmail.com>
2023-07-04 00:48:54 -0300

    Cria primeiro resolver e define entidade

---
+++ nest-graphql/src/pets/pets.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { Pet } from './entities/pet.entity';
+
+@Injectable()
+export class PetsService {
+  async findAll(): Promise<Pet[]> {
+    const pet = new Pet();
+
+    pet.id = 1;
+    pet.name = 'Mambo';
+
+    return [pet];
+  }
+}
